091ece72c3699fd8ece0bf036d6bf1fc9ad87097
Aaron Chen <pkuaaron@gmail.com>
2020-01-14 19:43:20 -0500

    add

--- tigbc/urls.py
+++ tigbc/urls.py
@@ -24,7 +24,6 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('sermon/', include('sermon.urls')),
+    path('', include('accountapp.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
 )
-
-
