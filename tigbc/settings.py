eb1ae8e92d98898c1444e5b80da9054f0d1ca9e8
Aaron Chen <pkuaaron@gmail.com>
2020-01-20 02:47:39 -0500

    add python-dateutil==2.8.1

--- tigbc/settings.py
+++ tigbc/settings.py
@@ -136,4 +136,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static', 'js'),
+    os.path.join(BASE_DIR, 'static', 'css'),
+    os.path.join(BASE_DIR, 'static', 'img'),
+    os.path.join(BASE_DIR, 'static', 'worship'),
 ]
