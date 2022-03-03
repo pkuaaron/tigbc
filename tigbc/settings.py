7ffdef0fbde62cc61fb1315b55d98c1e59492578
Min He <mhe8@ncsu.edu>
2022-03-03 00:54:01 -0500

    update the websites based on 2nd review

--- tigbc/settings.py
+++ tigbc/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%a*g#yk*sz6to%+9p=@!vi444ei3y6d@w)_^_4n8#a8z*ta@sy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['heroku.com', '127.0.0.1', 'tigbc.heroku.com', 'tigbc.herokuapp.com']
 
